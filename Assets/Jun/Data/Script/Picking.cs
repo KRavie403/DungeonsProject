2de6aaf622867b56db6fd4525e00d1fe4a88d432
DESKTOP-D3O6O7N\Atents <wns0685@gmail.com>
2023-04-05 12:58:20 +0900

    no message

---
+++ Assets/Jun/Data/Script/Picking.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Picking : MonoBehaviour
+{
+    public LayerMask pickMask;
+    public UnityEvent<Vector3> clickAction = null;
+
+    private GameBoard GB = null;
+    private Vector2Int currentHover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GB = GameObject.FindGameObjectWithTag("GameMapManager").GetComponent<GameBoard>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000.0f, pickMask))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if ((1 << hit.transform.gameObject.layer & pickMask) != 0)
+                {
+                    clickAction?.Invoke(hit.transform.position);
+                }
+
+            }
+            else
+            {
+                //Debug.Log(GB.GetTileIndex(hit.transform.gameObject));
+                Vector2Int hitPos = GB.GetTileIndex(hit.transform.gameObject);
+                if (currentHover == -Vector2Int.one)
+                {
+                    currentHover = hitPos;
+                    GB.tiles[hitPos.x, hitPos.y].layer = 8;
+                }
+                if (currentHover != hitPos)
+                {
+                    GB.tiles[currentHover.x, currentHover.y].layer = 3;
+                    currentHover = hitPos;
+                    GB.tiles[hitPos.x, hitPos.y].layer = 8;
+                }
+            }
+        }
+        else
+        {
+            if (currentHover != -Vector2Int.one)
+            {
+                GB.tiles[currentHover.x, currentHover.y].layer = 3;
+                currentHover = -Vector2Int.one;
+            }
+        }
+
+    }
+}
