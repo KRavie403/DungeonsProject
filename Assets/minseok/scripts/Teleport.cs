26ac0dabe5c6f138909fb9ba2cbe6529a43b1ba3
DESKTOP-8REQ8EP\Atents <oky1467@gmail.com>
2023-05-11 14:45:25 +0900

    텔포수정

--- Assets/minseok/scripts/Teleport.cs
+++ Assets/minseok/scripts/Teleport.cs
@@ -30,7 +30,7 @@ public class Teleport : MonoBehaviour
     Vector3 mypos = Vector3.zero;
     int x = 0, y = 0;
     
-    public void tp()
+    public void tp() //랜덤좌표로 이동시키기
     {
         x = Random.Range(1, 20);
         y = Random.Range(1, 20);
@@ -43,7 +43,6 @@ public class Teleport : MonoBehaviour
         while (GameManager.Inst.tiles[my_Pos.x, my_Pos.y].GetComponent<TileState>().isVisited < -1 ||
             GameManager.Inst.tiles[my_Pos.x, my_Pos.y].GetComponent<TileState>().isVisited == 0)
         {
-            Debug.Log("X"+ x + "Y" + y);
             x = Random.Range(1, 20);
             y = Random.Range(1, 20);
 
@@ -54,21 +53,33 @@ public class Teleport : MonoBehaviour
             mypos = new Vector3((float)my_Pos.x + half, 0, (float)my_Pos.y + half);
         }
 
-            pos = new Vector2Int(x, y);
-
-        foreach (var tile in tiles)
+        pos = new Vector2Int(x, y);
+        Vector2Int pos2 = new Vector2Int();
+        foreach (var tile in tiles) //이동
         {
             if (tile.my_obj == OB_TYPES.PLAYER)
             {
                 STATE _curState = tile.my_target.GetComponent<Player>().GetState();
                 if (_curState == STATE.ACTION)
                 {
+                    pos2.x = tile.my_target.GetComponent<Player>().my_Pos.x;
+                    pos2.y = tile.my_target.GetComponent<Player>().my_Pos.y;
+
                     GameObject obj1 = Instantiate(TPEffect, tile.my_target.transform.position, Quaternion.identity);
-                    
+
                     tile.my_target.transform.position = mypos;
                     tile.my_target.GetComponent<Player>().my_Pos = pos;
 
                     GameObject obj2 = Instantiate(TPEffect, tile.my_target.transform.position, Quaternion.identity);
+
+                    GameManager.Inst.tiles[pos.x, pos.y].GetComponent<TileState>().my_target = tile.my_target.gameObject;
+                    GameManager.Inst.tiles[pos.x, pos.y].GetComponent<TileState>().my_obj = tile.my_target.GetComponent<Player>().myType;
+                    GameManager.Inst.tiles[pos.x, pos.y].GetComponent<TileState>().isVisited = 0;
+                    GameManager.Inst.tiles[pos2.x, pos2.y].GetComponent<TileState>().my_target = null;
+                    GameManager.Inst.tiles[pos2.x, pos2.y].GetComponent<TileState>().my_obj = OB_TYPES.NONE;
+                    GameManager.Inst.tiles[pos2.x, pos2.y].GetComponent<TileState>().isVisited = -1;
+
+                    
                 }
             }
         }
