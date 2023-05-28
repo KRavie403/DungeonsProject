240fc0139e3c6036d8bda1d53379d6703c6de086
Hangbul <wns0685@gmail.com>
2023-05-28 14:21:25 +0900

    no message

--- Assets/Jun/Data/Script/Manager/MapManager.cs
+++ Assets/Jun/Data/Script/Manager/MapManager.cs
@@ -12,6 +12,7 @@ public class MapManager : Singleton<MapManager>
     public float scale = 1.0f;
 
     public LayerMask crashMask;
+    public LayerMask GroundMask;
     public Material tileMat;
     public Vector3 LBLocation = new Vector3(0, 0, 0);
 
@@ -35,8 +36,8 @@ public class MapManager : Singleton<MapManager>
                 Vector3 start = new Vector3(x + 0.5f, -0.5f, y + 0.5f);
                 Ray ray = new Ray(start, new Vector3(0, 1, 0));
 
-                if (!Physics.Raycast(ray, 10.0f, crashMask))
-                    tiles.Add(new Vector2Int(x, y), GenerateSingleTile(tileSize, x, y).GetComponent<TileStatus>());
+                if (Physics.Raycast(ray, 10.0f, ~crashMask))
+                        tiles.Add(new Vector2Int(x, y), GenerateSingleTile(tileSize, x, y).GetComponent<TileStatus>());
 
                 //obj.SetActive(false);
             }
